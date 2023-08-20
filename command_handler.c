d865b44be37b29b973c45e77f31d00d21e7358cc
raxidbou4nja <official.wetyn@gmail.com>
2023-08-20 16:30:57 -0700

    add command handler function

---
+++ command_handler.c
@@ -0,0 +1,44 @@
+#include "libshell.h"
+
+/**
+ * command_handler - handle command with functions.
+ * @token_count: token length
+ * @tokens: array of tokens
+ *
+ * Return: 0 if similar 1 if not
+ */
+
+void command_handler(int token_count, char *tokens[])
+{
+	int executed;
+	int exit_status;
+
+	if ((_strcmp(tokens[0], "env") == 0) && token_count == 1)
+	{
+		print_environment();
+		return;
+	}
+
+	if ((_strcmp(tokens[0], "exit") == 0))
+	{
+		if (token_count == 1)
+		{
+			exit_shell();
+		}
+
+		exit_status = _atoi(tokens[1]);
+
+		exit(exit_status);
+
+		return;
+}
+
+	executed = execute_without_path(tokens[0], tokens);
+
+	if (executed)
+	{
+		return;
+	}
+
+	executed = execute_with_path(tokens);
+}
