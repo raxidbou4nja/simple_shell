1d21dd87fc300a4957043a23ba6448e68b938ae6
derfal <sohail.bounnite@gmail.com>
2023-08-21 04:17:44 +0100

    fixing errors and finishing file

--- std_other_functions.c
+++ std_other_functions.c
@@ -61,3 +61,76 @@ void _strcpy(char *dest, const char *src)
 
 	*dest = '\0';
 }
+
+/**
+ * _strcat - append string to other
+ * @dest: Destination string
+ * @src: Source String
+ * Return: Pointer to the next token
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+	char *str = dest + _strlen(dest);
+
+	while (*src != '\0')
+	{
+		*str = *src;
+		str++;
+		src++;
+	}
+
+	*str = '\0';
+	return (dest);
+}
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: first string to compare.
+ * @s2: second string to compare.
+ *
+ * Return: 0 if similar 1 if not
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(const char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return (result * sign);
+}
