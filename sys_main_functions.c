59db17080b44fd269c9f67e35f0f3c1fc97bf834
derfal <sohail.bounnite@gmail.com>
2023-08-22 14:51:30 +0100

    adding funct env var

--- sys_main_functions.c
+++ sys_main_functions.c
@@ -167,3 +167,43 @@ char *_getenv(const char *input)
 	}
 	return (NULL);
 }
+
+/**
+ * _setenv - to update and change environment var
+ *
+ * @name: name of the environment variable.
+ * @value: value to set.
+ * @oWrite: indicating whether to overwrite
+ *
+ * Return: 0 on success, -1 if the maximum number of variables
+ */
+int _setenv(const char *name, const char *value, int oWrite)
+{
+	char env_vars[100][50];
+	char env_vals[100][100];
+	int env_count = 0;
+	int i;
+
+	for (i = 0; i < env_count; i++)
+	{
+		if (_strcmp(env_vars[i], name) == 0)
+		{
+			if (!oWrite)
+				return (0);
+			_strncpy(env_vals[i], value, 100 - 1);
+			return (0);
+		}
+	}
+
+	if (env_count < 100)
+	{
+		_strncpy(env_vars[env_count], name, 50 - 1);
+		_strncpy(env_vals[env_count], value, 100 - 1);
+		env_count++;
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
